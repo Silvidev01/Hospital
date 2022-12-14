e5be95663bb841e9176142d4fde4f9bbd9586e24
Silvidev01 <silvina.arteaga@gmail.com>
2022-12-14 20:45:48 -0300

    upload

--- src/Hospital/Main.java
+++ src/Hospital/Main.java
@@ -12,7 +12,14 @@ public class Main {
 
     public static void main (String [] args){
 
-        EmployeeSave EmployeeSave = new EmployeeSave();
+
+        EmployeeSave EmployeeSave = null;
+        try {
+            EmployeeSave = new EmployeeSave();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+        }
 
 
         //initializing employees
